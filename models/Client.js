533745a0f438254ec7837dc8ed797282116c83f4
Robin06670 <robinbianchi18@gmail.com>
2025-03-22 20:04:57 +0100

    Fix: rename client.js to Client.js for Linux compatibility

---
+++ models/Client.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const ClientSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // Lien vers l'utilisateur propriétaire
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  activity: {
+    type: String
+  },
+  fees: {
+    type: Number
+  },
+  charges: {
+    type: Number
+  },
+  collaborator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Collaborator" // Lien vers le collaborateur
+  },
+  isArchived: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model("Client", ClientSchema);
